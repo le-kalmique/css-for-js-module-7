097b59684722978735613e20afe2a8ed2349f22e
Renata <rdzhergalova@gmail.com>
2023-04-28 04:39:29 +0000

    Update Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, FAMILIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,7 +29,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
         <Logo />
+        <SubscribeAction>
+          <Button>
+            Subscribe
+          </Button>
+          <Link href="/#">Already a subscriber?</Link>
+        </SubscribeAction>
       </MainHeader>
     </header>
   );
@@ -39,6 +53,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +77,47 @@ const ActionGroup = styled.div`
   }
 `;
 
+const SubscribeAction = styled.div`
+  display: none;
+  justify-self: end;
+  align-self: end;
+
+  flex-direction: column;
+  align-items: center;
+  width: fit-content;
+  gap: 4px;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+  }
+`;
+
+const Link = styled.a`
+  font-family: ${FAMILIES.serif};
+  font-style: italic;
+  text-decoration: underline;
+  font-size: 0.875rem;
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  ${ActionGroup}, ${SubscribeAction} {
+    display: none;
+  }
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: 1fr max-content 1fr;
+
+    ${ActionGroup}, ${SubscribeAction} {
+      display: flex;
+    }
+  }
 `;
 
 export default Header;
