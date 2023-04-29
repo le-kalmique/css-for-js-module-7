6c1baf7a792bbcc4f013c5fe52a7140810756e6b
Renata <rdzhergalova@gmail.com>
2023-04-29 05:07:07 +0000

    4. Specialty Story grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { QUERIES } from '../../constants';
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 
 import MarketCard from '../MarketCard';
@@ -36,7 +37,9 @@ const SpecialtyStoryGrid = () => {
         </SectionTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+            <SportsStory>
+              <MiniStory key={data.id} {...data} />
+            </SportsStory>
           ))}
         </SportsStories>
       </SportsSection>
@@ -47,14 +50,49 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+    
+    > section:first-of-type {
+      padding-right: 16px;
+      border-right: 1px solid var(--color-gray-300);
+      margin-right: 16px;
+    }
+  }
 `;
 
 const MarketsSection = styled.section``;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns:
+    repeat(auto-fill, minmax(150px, 1fr));      
+`;
 
-const SportsSection = styled.section``;
+const SportsSection = styled.section`
+  display: grid;
+`;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns:
+    repeat(auto-fill, minmax(150px, 1fr));  
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    grid-template-columns: revert;
+    overflow: auto;
+  }
+`;
+
+const SportsStory = styled.div`
+  @media ${QUERIES.tabletAndUp} {
+    min-width: 220px;
+  }
+`
 
 export default SpecialtyStoryGrid;
